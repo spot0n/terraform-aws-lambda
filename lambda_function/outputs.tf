e1ac024f76dd46c241f0fddb12f13e31fad05c18
Brahm <brahm@finddoctoday.com>
2019-09-07 13:15:46 -0700

    Output lambda function name

--- lambda_function/outputs.tf
+++ lambda_function/outputs.tf
@@ -4,3 +4,6 @@ output "lambda_arn" {
 output "lambda_invoke_arn" {
   value = "${aws_lambda_function.lambda.invoke_arn}"
 }
+output "lambda_function_name" {
+  value = "${aws_lambda_function.lambda.function_name}"
+}
