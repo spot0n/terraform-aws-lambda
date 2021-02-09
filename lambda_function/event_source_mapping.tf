5e6c0b10e9fda1cd40dc0453fa046fa6a726496e
Brahm Kiran Singh <brahm@finddoctoday.com>
2021-02-09 11:14:41 -0800

    Allow maximum_batching_window_in_seconds to be specified for lambda

--- lambda_function/event_source_mapping.tf
+++ lambda_function/event_source_mapping.tf
@@ -1,7 +1,9 @@
 # Process events from SQS queue, DynamoDB, Kinesis
 resource "aws_lambda_event_source_mapping" "lambda" {
   count             = length(var.source_mappings)
-  batch_size        = lookup(var.source_mappings[count.index], "batch_size")
+  batch_size        = lookup(var.source_mappings[count.index], "batch_size", null)
+  maximum_batching_window_in_seconds = lookup(var.source_mappings[count.index], "maximum_batching_window_in_seconds", 60)
+  maximum_record_age_in_seconds = lookup(var.source_mappings[count.index], "maximum_record_age_in_seconds", 604800)
   event_source_arn  = lookup(var.source_mappings[count.index], "event_source_arn")
   enabled           = lookup(var.source_mappings[count.index], "enabled")
   function_name     = aws_lambda_function.lambda.arn
