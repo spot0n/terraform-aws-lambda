5ffe038b29a61991104a4d330c72006f9adb3df4
Brahm Kiran Singh <brahmkiran@gmail.com>
2021-06-19 08:25:09 -0700

    Set default value for maximum_record_age_in_seconds as null

--- lambda_function/event_source_mapping.tf
+++ lambda_function/event_source_mapping.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_event_source_mapping" "lambda" {
   count             = length(var.source_mappings)
   batch_size        = lookup(var.source_mappings[count.index], "batch_size", null)
   maximum_batching_window_in_seconds = lookup(var.source_mappings[count.index], "maximum_batching_window_in_seconds", 60)
-  maximum_record_age_in_seconds = lookup(var.source_mappings[count.index], "maximum_record_age_in_seconds", 604800)
+  maximum_record_age_in_seconds = lookup(var.source_mappings[count.index], "maximum_record_age_in_seconds", null)
   event_source_arn  = lookup(var.source_mappings[count.index], "event_source_arn")
   enabled           = lookup(var.source_mappings[count.index], "enabled")
   function_name     = aws_lambda_function.lambda.arn
